3952367bd6e3fdd071f7ed8b25e4467da89e7666
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-18 23:09:07 +0530

    making Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import { Link, NavLink } from "react-router-dom"
 const Header = () => {
   return (
     <div>
-      <h1>header</h1>
+      <header>
+        <div className="container">
+          <div className="d-flex justify-content-between align-items-center"></div>
+        </div>
+      </header>
     </div>
   )
 }
