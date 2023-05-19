90e7475d3350937f9e1a95faae99370e2d4976de
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-19 23:14:34 +0530

    adding data into the data.js in JSON format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,54 @@
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-    <div className='container'>
-      <div className='d-flex justify-content-between align-items-center'>
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="logo">
+            <Link to="/" className="brand">
+              Lovkush
+            </Link>
+          </div>
 
-        <div className='logo'>
-          <Link to='/' className='brand'>WSP</Link>
+          <nav className="nav nav-pills">
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="about"
+              className={(navData) =>
+                navData.isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="products"
+              className={(navData) =>
+                navData.isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Products
+            </NavLink>
+            <NavLink
+              to="posts"
+              className={(navData) =>
+                navData.isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Posts
+            </NavLink>
+          </nav>
         </div>
-
-        <nav className='nav nav-pills'>
-          <NavLink to='/' 
-            className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
-          >Home</NavLink>
-          <NavLink to='about'
-            className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
-          >About</NavLink>
-          <NavLink to='products'
-            className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
-          >Products</NavLink>
-          <NavLink to='posts'
-            className={(navData) => navData.isActive ? 'nav-link active' : 'nav-link'}
-          >Posts</NavLink>
-        </nav>
-
       </div>
-    </div>
-  </header>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
