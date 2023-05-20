b6bd0be27b4388afe145e734843bbe6029489db0
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-20 22:42:49 +0530

    completing the Posts page - by adding the states and loader into it

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Posts = () => {
+
+
+  //Creating useState
+  const [ posts, setPosts ] = useState([]);
+  const [ loader, setLoader ] = useState(true);
+
+
+  //for rendering giving the useEffect
+
+  useEffect(() => {
+    fetchPosts();
+  }, []); //useEffect has = callback function + [Dependency]
+
+
+  //Always use async await - as JS returns Promises
+  async function fetchPosts() {
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => setPosts(json))
+    .then(setLoader(false))
+  }
+
   return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+    <main className="posts">
+      <div className="pg-header">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-7">
+              <h1>Posts</h1>
+            </div>
+            <div className="col-lg-5">
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb justify-content-end">
+                  <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                  <li className="breadcrumb-item active" aria-current="page">Posts</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container content">
+        {loader
+          ? <div className="text-center">Loading...</div>
+          : (
+            <div className="row">
+              {posts.map(post => {
+                return(
+                  <div className="col-sm-6" key={post.id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">{post.title}</h5>
+                        <p className="card-text">
+                          {post.body}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        }
+      </div>
+    </main>
   )
 }
 
