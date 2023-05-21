9da04719d23aeb841f5bc34acb7ba6cff757f35e
LOVKUSH <lovkushyadav9888@gmail.com>
2023-05-21 12:11:52 +0530

    Completing the Eroor amd Linking it to the Home Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error</h1>
-    </div>
+    <main>
+      <div className='pg-header'>
+        <div className='container'>
+          <h1>404 Error</h1>
+        </div>
+      </div>
+      <div className='container content'>
+        <p>Page not found...</p>
+        <Link to = "/" className="btn btn-primary">Go Back to Homepage</Link>
+      </div>
+    </main>
   )
 }
 
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,1 +1,14 @@
 import { Outlet } from "react-router-dom";
+
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
+
+const MainLayout = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default MainLayout
